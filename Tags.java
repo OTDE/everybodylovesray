eddf0e85f58c5176e9fe332f40294ef62be6d2a5
Ethan Wiederspan <wiederem@plu.edu>
2018-10-22 19:24:29 -0700

    Initial Upload for Tags Class

---
+++ Tags.java
@@ -0,0 +1,14 @@
+
+/**
+ * @author EthanWiederspan & Seth Chapman
+ * Tags class holds String representations of various keywords 
+ * used in the project. This minimizes chance for error.
+ *
+ */
+public class Tags {
+
+	// Tags for output file types
+	public static final String PNG = "[PNG]";
+	public static final String HDR = "[HDR]";
+	
+}
