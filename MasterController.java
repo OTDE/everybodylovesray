7d2cd0d2704ae1f76cf706ff6d0cf5d1f80e9d8e
Ethan Wiederspan <wiederem@plu.edu>
2018-10-22 21:49:23 -0700

    Connected to file view and renderController, added beginRender Function

--- MasterController.java
+++ MasterController.java
@@ -1,4 +1,21 @@
 
 public class MasterController {
+	
+	private FileView fView;
+	private RenderController rendCon;
+	
+	public void promptForFile() {
+		
+	}
+	
+	public void beginRender() {
+		
+		// Make RenderController
+		rendCon = new RenderController(this);	
+		// TO-DO: Send File to JSON Parser
+		
+		// start display loop
+		rendCon.render();
+	}
 
 }
