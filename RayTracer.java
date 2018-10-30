eef19357260974d8eb8af700c1922978ff576fa5
Ethan Wiederspan <wiederem@plu.edu>
2018-10-29 21:06:55 -0700

    Update RayTracer.java

--- RayTracer.java
+++ RayTracer.java
@@ -3,6 +3,8 @@ import javafx.application.Application;
 public class RayTracer {
     
     public static void main(String[] args) {
-        Application.launch(FileView.class, args);
+    
+        MasterController mastCon = new MasterController();
+        mastCon.promptForFile();
     }
 }
