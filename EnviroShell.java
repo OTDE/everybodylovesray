ffe156534527bbd4c758ecb338314be8136ecb27
Seth Chapman <chapmasb@plu.edu>
2018-10-30 15:28:56 -0700

    Update EnviroShell.java

--- EnviroShell.java
+++ EnviroShell.java
@@ -1,1 +1,8 @@
+/**
+ * @author Ethan Wiederspan and Seth Chapman
+ * 
+ * Shell class for the GSON parser to use so it 
+ * can set up all the relevant fields using the
+ * JSON file: height and width.
+ */
 public class EnviroShell { public Environment environment; } 
