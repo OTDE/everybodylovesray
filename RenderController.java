62da2db63a2493982dc5eefbc7950734b21a7aef
Ethan Wiederspan <wiederem@plu.edu>
2018-10-29 19:41:05 -0700

    added basic render loop

--- RenderController.java
+++ RenderController.java
@@ -1,16 +1,45 @@
 
 public class RenderController {
 	
+	Film film;
+	
 	MasterController mastCon;
 	RenderView rendView;
+	boolean running = false;
 
 	public RenderController(MasterController mCon) {
 		this.mastCon = mCon;
+		film = new Film();
 	}
 
-	public void render() {
+	public void display() {
 		rendView = new RenderView(this);
 		
+	}
+	
+	public void startRendering() {
+		
+		Thread renderThread = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				running = true;
+				
+				// Updates the image displaying to the GUI after a certain increment of time
+				while(running) {
+					rendView.updateView(film.getRenderedImage());
+					try { Thread.sleep(2000); } catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				
+			}
+			
+		});
+	}// startRendering
+	
+	public void stopRendering() {
+		running = false;
 	}
 
 }
