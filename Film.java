0f976d8359c37460eb51055f62132030e7fa1a68
Ethan Wiederspan <wiederem@plu.edu>
2018-10-29 21:11:14 -0700

    Added necessary features for sprint01

--- Film.java
+++ Film.java
@@ -1,4 +1,15 @@
+import java.awt.image.BufferedImage;
 
 public class Film {
+	
+	private BufferedImage renderedImage;
+	
+	public Film(int width, int height) {
+		renderedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+	}
+	
+	public BufferedImage getRenderedImage() {
+		return renderedImage;
+	}
 
 }
