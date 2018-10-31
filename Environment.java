e6ac963e613969d88cbc03728763056f24e64795
Ethan Wiederspan <wiederem@plu.edu>
2018-10-31 01:21:59 -0700

    Additional Comments

--- Environment.java
+++ Environment.java
@@ -11,8 +11,8 @@ public class Environment {
 	public int height;
 	public int width;
 	
-	private transient Map<String, LightSource> lightMap;
-	private transient Map<String, Model> modelMap;
-	private transient Map<String, Material> matMap;
+	//private transient Map<String, LightSource> lightMap;
+	//private transient Map<String, Model> modelMap;
+	//private transient Map<String, Material> matMap;
 	
 }
