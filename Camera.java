f8358b34880e507b601f0acf7348c04cc5c9929e
Ethan Wiederspan <wiederem@plu.edu>
2018-11-13 13:52:26 -0800

    Added Camera Constructor

--- Camera.java
+++ Camera.java
@@ -1,3 +1,5 @@
+import org.joml.Matrix4d;
+import org.joml.Vector3d;
 
 /**
  * @author Ethan Wiederspan and Seth Chapman
@@ -8,19 +10,44 @@
 public class Camera {
 	
 	private Film film;
-	private double[] position; 
+	private Vector3d eye;
+	private Matrix4d rotation;
 	
-	public Camera(int at, int eye, int up, Film f) {
+	
+	public Camera(Vector3d pos, Vector3d at, Vector3d up, Film f) {
+		eye = pos;
+		
+		Vector3d n = eye.sub(at);
+		Vector3d u = up.cross(n);
+		Vector3d v = n.cross(u);
+ 		n.normalize();
+		u.normalize();
+		v.normalize();
+		
 		
 		this.film = f;
-		generatePosition(at, eye, up);
 	}
 
-	private void generatePosition(int at, int eye, int up) {
-		// convert input values to a position in 3d space
-		
+	private void setRotation(Vector3d u, Vector3d v, Vector3d n) {
+		rotation._m00(u.x); rotation._m01(u.y); rotation._m02(u.z);
+		rotation._m10(v.x); rotation._m11(v.y); rotation._m12(v.z);
+		rotation._m20(n.x); rotation._m21(n.y); rotation._m22(n.z);
 	}
 	
+	private Matrix4d viewMatrix() {
+		Matrix4d m = new Matrix4d();
+		m._m30(this.eye.x);
+		m._m30(this.eye.y);
+		m._m30(this.eye.z);
+		
+		this.rotation.mul(m,m);
+		
+		return m;
+	}
+	
+	public void generateRay( Sample samp) {
+		// method stub for future sprint
+	}
 	
 
 }
