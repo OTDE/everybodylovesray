94a529aac2e139db7549050eda0f38d773b96934
Seth Chapman <chapmasb@plu.edu>
2018-11-13 14:12:49 -0800

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,17 +1,32 @@
 
 
+/**
+ * Sample class for storing sample data.
+ * @author Ethan Wiederspan and Seth Chapman
+ *
+ */
 public class Sample {
 	private double offsetX;
 	private double offsetY;
+	
+	// These additional pieces of information regarding camera info have yet to be implemented.
 	// private int time;
 	// private double camX;
 	// private double camY;
 	
+	/**
+	 * Constructor for the sample class. Initializes sample information.
+	 * @param offX the amount by which a ray is offset in the x direction in a given pixel
+	 * @param offY the amount by which a ray is offset in the y direction in a given pixel
+	 */
 	public Sample(double offX, double offY) {
 		offsetX = offX;
 		offsetY = offY;
 	}
 
+	/**
+	 * Getters and setters
+	 */
 	public double getOffsetX() {
 		return offsetX;
 	}
