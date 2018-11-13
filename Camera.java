8e68e8f3408d93e5f982cb2659dc6ba273b577e2
Ethan Wiederspan <wiederem@plu.edu>
2018-11-13 14:59:01 -0800

    Added comments

--- Camera.java
+++ Camera.java
@@ -4,8 +4,9 @@ import org.joml.Vector3d;
 /**
  * @author Ethan Wiederspan and Seth Chapman
  * 
- * 
- *
+ * Camera class defines the position of view point of the scene, 
+ * also defines the rotation matrix needed to transform from world 
+ * coordinates to camera coordinates.
  */
 public class Camera {
 	
@@ -31,12 +32,25 @@ public class Camera {
 		this.film = f;
 	}
 
+	/**
+	 * Builds the camera's rotation matrix based on imput from JSON file
+	 * 
+	 * @param u vector defined by up cross n 
+	 * @param v vector defined by n cross u
+	 * @param n vector defined by eye - at
+	 */
 	private void setRotation(Vector3d u, Vector3d v, Vector3d n) {
 		rotation._m00(u.x); rotation._m01(u.y); rotation._m02(u.z);
 		rotation._m10(v.x); rotation._m11(v.y); rotation._m12(v.z);
 		rotation._m20(n.x); rotation._m21(n.y); rotation._m22(n.z);
 	}
 	
+	/*
+	 * Returns the view matrix of this camera generated via
+	 * the eye and rotation
+	 * 
+	 * @return Matrix4d containing the view matrix
+	 */
 	private Matrix4d viewMatrix() {
 		Matrix4d m = new Matrix4d();
 		m._m30(this.eye.x);
