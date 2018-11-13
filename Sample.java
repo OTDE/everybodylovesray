d2fbdfe7f8a63e4d14d001fb0550f6167b419b99
Seth Chapman <chapmasb@plu.edu>
2018-11-12 22:19:01 -0800

    Upload New File

---
+++ Sample.java
@@ -0,0 +1,22 @@
+
+
+public class Sample {
+	private double offsetX;
+	private double offsetY;
+	// private int time;
+	// private double camX;
+	// private double camY;
+	
+	public Sample(double offX, double offY) {
+		offsetX = offX;
+		offsetY = offY;
+	}
+
+	public double getOffsetX() {
+		return offsetX;
+	}
+
+	public double getOffsetY() {
+		return offsetY;
+	}
+}
