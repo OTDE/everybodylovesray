8db91a23be91d208445ef58e5176a86b14019a9f
Ethan Wiederspan <wiederem@plu.edu>
2018-11-27 22:45:53 -0800

    Initial Upload for Integrator

---
+++ src/main/java/Integrator.java
@@ -0,0 +1,20 @@
+import java.awt.Color;
+
+public class Integrator {
+	
+	private RenderController rendCon;
+	private Intersection inter;
+	
+	public Integrator(RenderController rCon) {
+		
+		this.rendCon = rCon;
+	}
+	
+	public Color propagate(Ray r) {
+		
+		
+		return Color.PINK;
+		
+	}
+
+}
