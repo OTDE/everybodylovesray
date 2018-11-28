86bd71ec5062dd6240f97c6ca46ea8fc5a9afb4e
Ethan Wiederspan <wiederem@plu.edu>
2018-11-27 23:53:10 -0800

    Initial upload for Intersection

---
+++ src/main/java/com/shffl/assets/Intersection.java
@@ -0,0 +1,13 @@
+import org.joml.Vector3d;
+
+public class Intersection {
+	
+	private Vector3d position;
+	// private Face face;
+	// private ObjModel obj;
+	
+	public Intersection() {
+		
+	}
+
+}
