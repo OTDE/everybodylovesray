d2451552701931c57599d00edb1064a1a7b422d0
EthanWiederspan <wiederem@plu.edu>
2019-02-28 19:42:27 -0800

    deleting unused file

--- src/main/java/com/shffl/assets/EnviroShell.java
+++
@@ -1,9 +0,0 @@
-package com.shffl.assets;
-/**
- * @author Ethan Wiederspan and Seth Chapman
- * 
- * Wrapper class for the GSON parser to use so it 
- * can set up all the relevant fields using the
- * JSON file.
- */
-public class EnviroShell { public Scene scene; } 
